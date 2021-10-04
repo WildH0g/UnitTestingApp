fdb788e3666e86906f19d92b92c8bfb824fa0a56
dlealv <dleal67@gmail.com>
2021-10-04 13:41:00 -0400

    Update TestForVersion_0.1.1.js
    Updated the file, with additional tests, including when input argument is a condition (not a funcion) and when input argument are `undefined`

--- Example/TestForVersion_0.1.1.js
+++ Example/TestForVersion_0.1.1.js
@@ -35,14 +35,28 @@ function runTests() {
   // Testing new method: assertEquals, printSummary and printSubHeader using addTwoValue function with default levelInfo
   // We haven´t set levelInfo, so we are using the default value: 1
   test.printSubHeader("Using default Level Info value (1) for 'addTwoValues' function");
+  test.printSubHeader("Test 1 passes, Test 2 passes with message, Test 3 fails, Test 4 fails with custom message");
   test.assertEquals(() => addTwoValues(1,2),3); // PASS
   test.assertEquals(() => addTwoValues(2,2),4, "Valid case: 2 + 2 = 4"); // PASS with message
   test.assertEquals(() => addTwoValues(1,2),4); // FAIL
   test.assertEquals(() => addTwoValues(1,2),4, "Expected to fail, because 1+2 != 4");
   test.printSummary(); // It should print final result and statistics (two lines)
+  test.printSubHeader("Testing when fun is boolean");
+   test.printSubHeader("Reset counters");
+  test.resetTestCounters();
+  test.printSubHeader("Test-1 Pass, Test-2 Fail");
+  test.assertEquals(1 + 2 == 3, true);
+  test.assertEquals(1 + 1 == 3, true);
+  test.printSummary();
+  test.printSubHeader("Testing undefined input arguments");
+  test.assertEquals();
+  test.assertEquals(1+1);
+  test.printSubHeader("Expected to see 4-Tests, 1-Pass and 3-Fail");
+  test.printSummary();
+
 
   // Testing the same tests but in silent mode (LevelInfo = 0)
-  test.printSubHeader("Testing the output setting levelInfo = 0"); // It logs out, because we haven't changed LevelInfo yet
+  test.printSubHeader("Testing the same tests with setting levelInfo = 0"); // It logs out, because we haven't changed LevelInfo yet
   test.setLevelInfo(0); // 0-Only summary result, 1-Detail results
   // Because LevelInfo = 0 we use the console for traceability purpose
   console.log("LevelInfo = " + test.getLevelInfo());
@@ -51,19 +65,37 @@ function runTests() {
   test.assertEquals(() => addTwoValues(1,2),4);
   test.assertEquals(() => addTwoValues(1,2),4, "Expected to fail, because 1+2 != 4");
   test.printSummary(); // Only summary result: Some test failed
-  console.log("Reset counters");
+  test.printSubHeader("Reset counters");
   test.resetTestCounters();
   console.log("Shows only the summary line, the counters are reseted so no tests, therefore all test passed");
   test.printSummary(); //Shows only summary line
 
   // Testing the existing assert function work as expected
-  test.printHeader("Testing addTwoValues using assert");
   test.printSubHeader('Default level Info');
   test.setLevelInfo(1); 
+  test.printHeader("Testing addTwoValues using assert");
   test.printSubHeader("LevelInfo = " + test.getLevelInfo());
+  test.printSubHeader("Test-1 Pass, Test-2 Fail");
   test.assert(() => addTwoValues(1,2) == 3, "Valid case: 1 + 2 = 3");
   test.assert(() => addTwoValues(1,2) == 4, "Invalid case: 1 + 2 != 4");
   test.printSubHeader("Expected to log out two lines: statistics and summary");
+  test.printSummary();
+
+  test.printSubHeader("Testing when condition is boolean");
+  test.printSubHeader("Reset counters");
+  test.resetTestCounters();
+  test.printSubHeader("Test-1 Pass, Test-2 Fail");
+  test.assert(1 + 2 == 3, "Valid case: 1 + 2 = 3");
+  test.assert(1 + 1 == 3, "Invalid case: 1 + 1 != 3");
+  test.printSummary();
+
+  test.printSubHeader("Testing undefined input arguments");
+  test.printSubHeader("Reset counters");
+  test.resetTestCounters();
+  test.printSubHeader("Expected to see 3-Tests. Test-1 Pass, Test-2 Pass and Test-3 Fail");
+  test.assert();
+  test.assert(1+1==2);
+  test.assert(1+1==3);
   test.printSummary();
 
   test.printSubHeader("Testing under silent mode: one test passed, one failed");
